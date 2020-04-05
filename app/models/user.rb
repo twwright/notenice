4b88c6195550fbe2c33f5aea883369357c8f1dbc
twwright <twwright@gmail.com>
2020-04-05 17:36:54 -0500

    Add associations and validations to model

---
+++ app/models/user.rb
@@ -0,0 +1,16 @@
+class User < ActiveRecord::Base
+	has_many :drugs
+	has_many :side_effects, through :drugs
+	has_secure_password
+	validates :username, presence: true, uniqueness: true, length: { minimum: 3 }
+	validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+	validates :password, length: { in: 6..20 }
+
+	# def uses_drug?(drug)
+		# returns true when the drug has been used by the user
+	# end
+
+	# def has_side_effect?(side_effect)
+		# returns true when the user has the side effect
+	# end
+end
