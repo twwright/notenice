5ba98f67314bd6c85c4646386888aa47dbd3cd31
twwright <twwright@gmail.com>
2020-04-20 02:12:37 -0500

    Fix issue with code block error status

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -59,33 +59,23 @@ class ApplicationController < Sinatra::Base
 			@note = Note.find_by_id(params[:id])
 		end
 
-        
-        class CodeRayify < Redcarpet::Render::HTML
-            def block_code(code, language)
-                CodeRay.scan(code, language).div
-            end
-        end
+		  
+		  class CodeRayify < Redcarpet::Render::HTML
+				def block_code(code, language)
+					 CodeRay.scan(code, language || :text).div
+				end
+		  end
 
-        def markdown(text)
-            coderayified = CodeRayify.new(:filter_html => true, :hard_wrap => true)
-            options = {
-             :fenced_code_blocks => true,
-             :no_intra_emphasis => true,
-             :autolink => true,
-             :lax_html_blocks => true,
-            }
-            markdown_to_html = Redcarpet::Markdown.new(coderayified, options)
-            markdown_to_html.render(text)
-        end
+		  def markdown(text)
+				coderayified = CodeRayify.new(:filter_html => true, :hard_wrap => true)
+				options = {
+				 :fenced_code_blocks => true,
+				 :no_intra_emphasis => true,
+				 :autolink => true,
+				 :lax_html_blocks => true,
+				}
+				markdown_to_html = Redcarpet::Markdown.new(coderayified, options)
+				markdown_to_html.render(text)
+		  end
 	end
 end
-
-=begin
-FEATURES:
-	- KEEP TRACK OF COPIES VS ORIGINAL
-	- ALLOW PUSH OF COPIES INTO ORIGINAL, GIT-STYLE
-	- ALLOW COLLABORATORS TO BE ADDED TO A NOTE
-	- 
-
-
-=end
